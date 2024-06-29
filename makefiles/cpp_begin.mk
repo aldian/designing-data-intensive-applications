49a4cc60e0b3a24c2094e7b0f5c43d3ef1b437ef
Aldian Fazrihady <aldian.f@ntucenterprise.sg>
2024-06-29 18:00:39 +0700

    Refactor makefiles

---
+++ makefiles/cpp_begin.mk
@@ -0,0 +1,3 @@
+CXX = g++
+CXXFLAGS = -std=c++11 -Wall -fprofile-arcs -ftest-coverage
+LDFLAGS = -lgcov --coverage -lgtest -lgtest_main
---
+++ makefiles/cpp_end.mk
@@ -0,0 +1,27 @@
+OBJECTS = $(SOURCES:.cpp=.o)
+
+all: $(TARGET)
+
+$(TARGET): $(OBJECTS)
+	$(CXX) $(OBJECTS) -o $(TARGET) $(LDFLAGS)
+
+%.o: %.cpp %.h
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJECTS) $(TARGET) *.gcda *.gcno coverage.info
+	rm -rf out
+
+coverage: all run_tests generate_report
+
+run_tests: $(TARGET)
+	./$(TARGET)
+
+generate_report:
+	lcov --capture --directory . --output-file coverage.info --no-external --rc lcov_branch_coverage=1
+	lcov --remove coverage.info '/usr/*' '*test_*' --output-file coverage.info
+	genhtml coverage.info --output-directory out
+	@echo "Coverage report generated in out/index.html"
+
+format:
+	clang-format -i *.cpp *.h
